75c86126ef21c5855796cd0fee0b6a1a9256f640
oniko94 <onikolaiev94@outlook.com>
2025-03-20 10:28:00 +0200

    WIP: Improve structure

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,5 +1,7 @@
-local pl_strings = require("plenary.strings")
 local lualine = require("lualine")
+local pl_strings = require("plenary.strings")
+
+local color_scheme = require("config.colors")
 
 local conditions = {
     buffer_not_empty = function ()
@@ -15,103 +17,6 @@ local conditions = {
     end
 }
 
-local colors = {
-    bg = "#343434",
-    fg = "#cdcdcd",
-    yellow = "#cdcd00",
-    cyan = "#007ccd",
-    darkblue = "#00009a",
-    green = "#00cd00",
-    orange = "#ffa500",
-    violet = "#7c00cd",
-    magenta = "#cd00cd",
-    blue = "#0000cd",
-    red = "#cd0000",
-}
-
-local mode_color = {
-    n = {
-        bg = colors.orange,
-        fg = colors.bg
-    },
-    i = {
-        bg = colors.cyan,
-        fg = colors.bg
-    },
-    v = {
-        bg = colors.yellow,
-        fg = colors.bg
-    },
-    [''] = {
-        bg = colors.blue,
-        fg = colors.fg
-    },
-    V = {
-        bg = colors.blue,
-        fg = colors.fg
-    },
-    c = {
-        bg = colors.magenta,
-        fg = colors.fg
-    },
-    no = {
-        bg = colors.red,
-        fg = colors.fg
-    },
-    s = {
-        bg = colors.cyan,
-        fg = colors.bg
-    },
-    S = {
-        bg = colors.cyan,
-        fg = colors.bg
-    },
-    [''] = {
-        bg = colors.cyan,
-        fg = colors.bg
-    },
-    ic = {
-        bg = colors.yellow,
-        fg = colors.bg
-    },
-    R = {
-        bg = colors.violet,
-        fg = colors.fg
-    },
-    Rv = {
-        bg = colors.violet,
-        fg = colors.fg
-    },
-    cv = {
-        bg = colors.red,
-        fg = colors.fg
-    },
-    ce = {
-        bg = colors.red,
-        fg = colors.fg
-    },
-    r = {
-        bg = colors.cyan,
-        fg = colors.bg
-    },
-    rm = {
-        bg = colors.cyan,
-        fg = colors.bg
-    },
-    ['r?'] = {
-        bg = colors.cyan,
-        fg = colors.bg
-    },
-    ['!'] = {
-        bg = colors.red,
-        fg = colors.fg
-    },
-    t = {
-        bg = colors.red,
-        fg = colors.fg
-    },
-}
-
 local config = {
     options = {
         component_separators = { left = '', right = ''},
@@ -119,12 +24,12 @@ local config = {
         section_separators = { left = '', right = ''},
         theme = {
             normal = {
-                a = { fg = colors.fg, bg = colors.bg },
-                b = { fg = colors.fg, bg = colors.bg },
-                c = { fg = colors.fg, bg = colors.bg },
-                x = { fg = colors.fg, bg = colors.bg },
-                y = { fg = colors.fg, bg = colors.bg },
-                z = { fg = colors.fg, bg = colors.bg }
+                a = { fg = color_scheme.palette.fg, bg = color_scheme.palette.bg },
+                b = { fg = color_scheme.palette.fg, bg = color_scheme.palette.bg },
+                c = { fg = color_scheme.palette.fg, bg = color_scheme.palette.bg },
+                x = { fg = color_scheme.palette.fg, bg = color_scheme.palette.bg },
+                y = { fg = color_scheme.palette.fg, bg = color_scheme.palette.bg },
+                z = { fg = color_scheme.palette.fg, bg = color_scheme.palette.bg }
             }
         }
     },
@@ -134,13 +39,13 @@ local config = {
                 function()
                     return "▊"
                 end,
-                color = { fg = colors.yellow },
+                color = { fg = color_scheme.palette.yellow },
                 padding = { left = 0, right = 1 },
             },
             {
                 "mode",
                 color = function ()
-                    return mode_color[vim.fn.mode()]
+                    return color_scheme.editor_mode[vim.fn.mode()]
                 end,
                 padding = { left = 1, right = 1 }
             }
@@ -151,15 +56,15 @@ local config = {
                 fmt = function(str)
                     return pl_strings.truncate(str, 30)
                 end,
-                color = { fg = colors.yellow, bg = colors.darkblue, gui = "bold" },
+                color = { fg = color_scheme.palette.yellow, bg = color_scheme.palette.darkblue, gui = "bold" },
                 cond = conditions.check_git_workspace,
             },
             {
                 "diff",
                 diff_color = {
-                    added = { fg = colors.green },
-                    modified = { fg = colors.orange },
-                    removed = { fg = colors.red },
+                    added = { fg = color_scheme.palette.green },
+                    modified = { fg = color_scheme.palette.orange },
+                    removed = { fg = color_scheme.palette.red },
                 },
                 cond = conditions.check_git_workspace,
             },
@@ -169,7 +74,7 @@ local config = {
             {
                 "filename",
                 cond = conditions.buffer_not_empty,
-                color = { fg = colors.magenta, gui = "bold" },
+                color = { fg = color_scheme.palette.magenta, gui = "bold" },
                 path = 1
             },
         },
@@ -178,13 +83,13 @@ local config = {
                 "encoding",
                 fmt = string.upper,
                 cond = conditions.hide_in_width,
-                color = { fg = colors.fg, gui = 'bold' },
+                color = { fg = color_scheme.palette.fg, gui = 'bold' },
             },
             {
                 "fileformat",
                 fmt = string.upper,
                 icons_enabled = false,
-                color = { fg = colors.yellow, gui = "bold" }
+                color = { fg = color_scheme.palette.yellow, gui = "bold" }
             }
         },
         lualine_y = {
@@ -192,8 +97,8 @@ local config = {
                 "datetime",
                 style = "%d %b, %Y %H:%M:%S",
                 color = {
-                    fg = colors.bg,
-                    bg = colors.yellow,
+                    fg = color_scheme.palette.bg,
+                    bg = color_scheme.palette.yellow,
                     gui = "bold"
                 }
             },
